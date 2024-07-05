6a67c081e134423768ce20ffc49f6976f182c669
Mushmat <edu.chirayu2005@gmail.com>
2024-07-05 16:35:37 +0530

    Implemented the AuthScreen Composable which checks whether the user has signed up or not. If signed up and logged in, it welcomes the user, else sends to the required screen for authentication.

--- app/src/main/java/com/example/wigglesapp/MainActivity.kt
+++ app/src/main/java/com/example/wigglesapp/MainActivity.kt
@@ -9,39 +9,33 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.wigglesapp.ui.theme.WigglesAppTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            WigglesAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+
+@Composable
+fun AuthScreen(authViewModel: AuthViewModel = viewModel()) {
+    var isSignUp by remember { mutableStateOf(false) }
+    val authState by authViewModel.authState.collectAsState()
+
+    if(authState.isAuthenticated){
+        Text(text = "Welcome to Wiggles!")
+    }else{
+        if(isSignUp){
+            SignUpScreen(authViewModel) {isSignUp = false }
+        } else {
+            LoginScreen(authViewModel) {isSignUp = true}
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WigglesAppTheme {
-        Greeting("Android")
-    }
-}
+fun LoginScreen
