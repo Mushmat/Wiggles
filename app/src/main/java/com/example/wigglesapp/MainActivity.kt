48833f43682a5e7a0a7d66c9a1b1b846b1ec1a00
Mushmat <edu.chirayu2005@gmail.com>
2024-07-05 16:42:57 +0530

    Currently building the Login Screen. An icon and the title has been implemented. Two text fields for email id and password have been implemented. The password has been transformed to the aestrics format.

--- app/src/main/java/com/example/wigglesapp/MainActivity.kt
+++ app/src/main/java/com/example/wigglesapp/MainActivity.kt
@@ -4,18 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.wigglesapp.ui.theme.WigglesAppTheme
 
@@ -38,4 +51,33 @@ fun AuthScreen(authViewModel: AuthViewModel = viewModel()) {
 }
 
 @Composable
-fun LoginScreen
+fun LoginScreen(authViewModel: AuthViewModel, onSignUpClicked: () -> Unit){
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(painter = painterResource(id = R.drawable.baseline_pets_24), contentDescription = "Cute Pets")
+        Text(text = "Wiggles", style = MaterialTheme.typography.titleLarge, color = Color.Blue)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = email,
+            onValueChange = {email = it},
+            label = { Text(text = "Email")},
+            modifier = Modifier.fillMaxWidth(0.8f)
+        )
+
+        TextField(
+            value = password,
+            onValueChange = {password = it},
+            label = { Text(text = "Password")},
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth(0.8f)
+        )
+    }
+}
