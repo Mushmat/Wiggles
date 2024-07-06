3b53935d4e7db0dc0a859e480b2b698554794f2f
Mushmat <edu.chirayu2005@gmail.com>
2024-07-06 15:58:34 +0530

    Implemented the navigation drawer and all the eight drawer items. Also loaded the icons for the items from the resource manager!

--- app/src/main/java/com/example/wigglesapp/NavigationDrawer.kt
+++ app/src/main/java/com/example/wigglesapp/NavigationDrawer.kt
@@ -1,10 +1,15 @@
 package com.example.wigglesapp
 
+import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -16,6 +21,61 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+
+@Composable
+fun NavigationDrawer(navController: NavController){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    ) {
+        DrawerMenuItem(text = "About Us", icon = R.drawable.baseline_info_24) {
+            navController.navigate("aboutUs")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        DrawerMenuItem(text = "User Profile", icon = R.drawable.person) {
+            navController.navigate("userProfile")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        DrawerMenuItem(text = "Adoption Process Instructions", icon = R.drawable.instructions) {
+            navController.navigate("adoptionProcessInstructions")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        DrawerMenuItem(text = "Bookmarked Pets", icon = R.drawable.bookmark) {
+            navController.navigate("bookmarkedPets")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        DrawerMenuItem(text = "Adoption Application Tracker", icon = R.drawable.tracker) {
+            navController.navigate("adoptionApplicationTracker")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        DrawerMenuItem(text = "Testimonials", icon = R.drawable.feedback) {
+            navController.navigate("testimonials")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        DrawerMenuItem(text = "FAQs", icon = R.drawable.answer) {
+            navController.navigate("faqs")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        DrawerMenuItem(text = "Contact Us", icon = R.drawable.contact) {
+            navController.navigate("contactUs")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+    }
+}
+
 
 @Composable
 fun DrawerMenuItem(text: String, icon: Int, onClick: () -> Unit){
