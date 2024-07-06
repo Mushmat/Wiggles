6578c75e2b3f9b3b83e260b18ace44f5489f4330
Mushmat <edu.chirayu2005@gmail.com>
2024-07-06 16:05:11 +0530

    Finished set up of the main acitivity.

--- app/src/main/java/com/example/wigglesapp/MainActivity.kt
+++ app/src/main/java/com/example/wigglesapp/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.wigglesapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.compose.rememberNavController
 import com.example.wigglesapp.ui.theme.WigglesAppTheme
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity(){
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContent{
@@ -18,6 +30,28 @@ class MainActivity : ComponentActivity(){
                 val navController = rememberNavController()
                 val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
                 val scope = rememberCoroutineScope()
+
+                ModalNavigationDrawer(
+                    drawerState = drawerState,
+                    drawerContent = {
+                        NavigationDrawer(navController = navController)
+                    }
+                ) {
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = { Text(text = "Wiggles") },
+                                navigationIcon = {
+                                    IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                                        Icon(painter = painterResource(id = R.drawable.baseline_menu_24), contentDescription = "Menu")
+                                    }
+                                }
+                            )
+                        }
+                    ) {
+                        HomeScreen(navController = navController)
+                    }
+                }
             }
         }
     }
