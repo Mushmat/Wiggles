ca34081718689252a0987355bcf544fdaa94ae3f
Mushmat <edu.chirayu2005@gmail.com>
2024-07-06 13:23:59 +0530

    In the home screen, defined a scaffold with a top bar that says WIGGLES and contains an Icon Button to navigate to the drawer screen.

--- app/src/main/java/com/example/wigglesapp/HomeScreen.kt
+++ app/src/main/java/com/example/wigglesapp/HomeScreen.kt
@@ -1,5 +1,43 @@
 package com.example.wigglesapp
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 @Composable
+fun HomeScreen(navController: NavController){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Wiggles") },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO: OPEN NAVIGATION DRAWER HERE*/ }) {
+                        Icon(painter = painterResource(id = R.drawable.baseline_menu_24),
+                            contentDescription = "Menu")
+                    }
+                }
+            )
+        },
+        content = {
+            Column (
+                modifier = Modifier.fillMaxSize().padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+
+            }
+        }
+    )
+}
