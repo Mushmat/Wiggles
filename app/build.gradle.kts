f86510e8274fd483b770e0a8538f38a43c0b2248
Mushmat <edu.chirayu2005@gmail.com>
2024-07-06 16:44:39 +0530

    Slight changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 
 dependencies {
 
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
