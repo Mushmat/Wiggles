61d0d485801987ff315f6b6a75defcd2ec344f2e
Mushmat <edu.chirayu2005@gmail.com>
2024-07-06 15:48:44 +0530

    Added how the drawer menu item looks like.

--- app/src/main/java/com/example/wigglesapp/NavigationDrawer.kt
+++ app/src/main/java/com/example/wigglesapp/NavigationDrawer.kt
@@ -1,2 +1,33 @@
 package com.example.wigglesapp
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun DrawerMenuItem(text: String, icon: Int, onClick: () -> Unit){
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Image(painter = painterResource(id = icon), contentDescription = text, modifier = Modifier.size(48.dp))
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = text, style = MaterialTheme.typography.bodyLarge.copy(fontSize = 20.sp))
+    }
+}
