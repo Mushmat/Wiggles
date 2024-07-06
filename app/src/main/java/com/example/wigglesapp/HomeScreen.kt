72a0b409a198c1cc42ca9dd08aa971e1cab10f65
Mushmat <edu.chirayu2005@gmail.com>
2024-07-06 13:27:47 +0530

    Defined five Home Option Items with their names and the navigation route that will be used when the user clicks on any of them.

--- app/src/main/java/com/example/wigglesapp/HomeScreen.kt
+++ app/src/main/java/com/example/wigglesapp/HomeScreen.kt
@@ -2,8 +2,11 @@ package com.example.wigglesapp
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -16,6 +19,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController){
     Scaffold(
@@ -32,11 +36,35 @@ fun HomeScreen(navController: NavController){
         },
         content = {
             Column (
-                modifier = Modifier.fillMaxSize().padding(16.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ){
+                HomeOptionItem("Available Pets", R.drawable.baseline_pets_24){
+                    navController.navigate("availablePets")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
 
+                HomeOptionItem("Don't know which pet to go with?", R.drawable.baseline_pets_24){
+                    navController.navigate("findBestPet")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+
+                HomeOptionItem("Pet Care", R.drawable.baseline_pets_24){
+                    navController.navigate("petCare")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+
+                HomeOptionItem("Shelter Info", R.drawable.baseline_pets_24){
+                    navController.navigate("shelterInfo")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+
+                HomeOptionItem("Donate", R.drawable.baseline_pets_24){
+                    navController.navigate("donate")
+                }
             }
         }
     )
