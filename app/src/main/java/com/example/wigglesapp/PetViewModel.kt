b9cf1dd05851398f78f8e5d0e7f75716e78435ff
Mushmat <edu.chirayu2005@gmail.com>
2024-07-07 22:02:08 +0530

    Created the pet view model for the bookmark functions and the repository class initiation.

---
+++ app/src/main/java/com/example/wigglesapp/PetViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.wigglesapp
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+class PetViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repository: PetRepository
+    val bookmarkedPets: Flow<List<PetEntity>>
+
+    init {
+        val petDao = PetDatabase.getDatabase(application).petDao()
+        repository = PetRepository(petDao)
+        bookmarkedPets = repository.bookmarkedPets
+    }
+
+    fun bookmarkPet(pet:Pet) = viewModelScope.launch {
+        repository.bookmarkPet(pet.toEntity())
+    }
+    fun removeBookmark(petId: Int) = viewModelScope.launch {
+        repository.removeBookmark(petId)
+    }
+
+}
