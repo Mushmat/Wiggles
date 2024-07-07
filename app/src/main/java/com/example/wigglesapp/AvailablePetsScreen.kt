1ef4b55a6f0615e062f567c9145ac343c13c361b
Mushmat <edu.chirayu2005@gmail.com>
2024-07-07 13:30:04 +0530

    Initiated the making of the Available Pets Screen

---
+++ app/src/main/java/com/example/wigglesapp/AvailablePetsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.wigglesapp
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun AvailablePetsScreen(navController: NavController){
+
+}
