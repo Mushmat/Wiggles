f95bfc9e9edfe60d1286511f8acc1060121b6727
Mushmat <edu.chirayu2005@gmail.com>
2024-07-07 21:07:06 +0530

    Fixed the filtering function where only one filter option was available to select.

--- app/src/main/java/com/example/wigglesapp/FilterScreen.kt
+++ app/src/main/java/com/example/wigglesapp/FilterScreen.kt
@@ -1,29 +1,11 @@
 package com.example.wigglesapp
 
-import android.widget.Space
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -31,10 +13,10 @@ import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FilterScreen(navController: NavController, applyFilters: (String, String, String) -> Unit) {
-    var selectedBreed by remember { mutableStateOf("") }
-    var selectedGender by remember { mutableStateOf("") }
-    var selectedSize by remember { mutableStateOf("") }
+fun FilterScreen(navController: NavController, applyFilters: (List<String>, List<String>, List<String>) -> Unit) {
+    val selectedBreeds = remember { mutableStateListOf<String>() }
+    val selectedGenders = remember { mutableStateListOf<String>() }
+    val selectedSizes = remember { mutableStateListOf<String>() }
 
     val breeds = listOf("Pit Bull Terrier", "Dachshund", "Plott Hound", "Hound", "Mixed Breed", "Labrador Retriever", "Jack Russell Terrier", "Australian Cattle Dog", "German Shepherd", "Domestic Short Hair", "American Short Hair", "Domestic Medium Hair", "Abyssinian", "Bengal", "British Short Hair", "Dilute Calico")
     val genders = listOf("Male", "Female")
@@ -51,24 +33,26 @@ fun FilterScreen(navController: NavController, applyFilters: (String, String, St
                 }
             )
         }
-    ) {
-        paddingValues ->
+    ) { paddingValues ->
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
                 .padding(16.dp)
         ) {
-            item{
+            item {
                 Text(text = "Select Breed")
-                breeds.forEach{
-                        breed ->
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        RadioButton(
-                            selected = selectedBreed == breed,
-                            onClick = { selectedBreed = breed }
+                breeds.forEach { breed ->
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Checkbox(
+                            checked = selectedBreeds.contains(breed),
+                            onCheckedChange = { checked ->
+                                if (checked) {
+                                    selectedBreeds.add(breed)
+                                } else {
+                                    selectedBreeds.remove(breed)
+                                }
+                            }
                         )
                         Text(text = breed)
                     }
@@ -77,14 +61,17 @@ fun FilterScreen(navController: NavController, applyFilters: (String, String, St
                 Spacer(modifier = Modifier.height(16.dp))
 
                 Text(text = "Select Gender")
-                genders.forEach{
-                        gender ->
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        RadioButton(
-                            selected = selectedBreed == gender,
-                            onClick = { selectedBreed = gender }
+                genders.forEach { gender ->
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Checkbox(
+                            checked = selectedGenders.contains(gender),
+                            onCheckedChange = { checked ->
+                                if (checked) {
+                                    selectedGenders.add(gender)
+                                } else {
+                                    selectedGenders.remove(gender)
+                                }
+                            }
                         )
                         Text(text = gender)
                     }
@@ -93,14 +80,17 @@ fun FilterScreen(navController: NavController, applyFilters: (String, String, St
                 Spacer(modifier = Modifier.height(16.dp))
 
                 Text(text = "Select Size")
-                sizes.forEach{
-                        size ->
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        RadioButton(
-                            selected = selectedBreed == size,
-                            onClick = { selectedBreed = size }
+                sizes.forEach { size ->
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Checkbox(
+                            checked = selectedSizes.contains(size),
+                            onCheckedChange = { checked ->
+                                if (checked) {
+                                    selectedSizes.add(size)
+                                } else {
+                                    selectedSizes.remove(size)
+                                }
+                            }
                         )
                         Text(text = size)
                     }
@@ -109,7 +99,7 @@ fun FilterScreen(navController: NavController, applyFilters: (String, String, St
                 Spacer(modifier = Modifier.height(16.dp))
 
                 Button(onClick = {
-                    applyFilters(selectedBreed, selectedGender, selectedSize)
+                    applyFilters(selectedBreeds, selectedGenders, selectedSizes)
                     navController.popBackStack()
                 }) {
                     Text(text = "Apply Filters")
