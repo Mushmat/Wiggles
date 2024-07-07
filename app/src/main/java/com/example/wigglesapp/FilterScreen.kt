d05e562c2d8012d0a1f6325b8f87f1913c59308d
Mushmat <edu.chirayu2005@gmail.com>
2024-07-07 20:21:58 +0530

    Created three filter properties: Breeds, Genders and Sizes

--- app/src/main/java/com/example/wigglesapp/FilterScreen.kt
+++ app/src/main/java/com/example/wigglesapp/FilterScreen.kt
@@ -1,2 +1,20 @@
 package com.example.wigglesapp
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavController
+
+@Composable
+fun FilterScreen(navController: NavController, applyFilters: (String, String, String) -> Unit) {
+    var selectedBreed by remember { mutableStateOf("") }
+    var selectedGender by remember { mutableStateOf("") }
+    var selectedSize by remember { mutableStateOf("") }
+
+    val breeds = listOf("Pit Bull Terrier", "Dachshund", "Plott Hound", "Hound", "Mixed Breed", "Labrador Retriever", "Jack Russell Terrier", "Australian Cattle Dog", "German Shepherd", "Domestic Short Hair", "American Short Hair", "Domestic Medium Hair", "Abyssinian", "Bengal", "British Short Hair", "Dilute Calico")
+    val genders = listOf("Male", "Female")
+    val sizes = listOf("Small", "Medium")
+
+}
