11c67a4b4f18f371e2a61acaa724bd02c71aafc6
Mushmat <edu.chirayu2005@gmail.com>
2024-07-08 20:16:44 +0530

    Minor Changes

--- app/src/main/java/com/example/wigglesapp/PetQuizScreen.kt
+++ app/src/main/java/com/example/wigglesapp/PetQuizScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -64,6 +66,7 @@ fun PetQuizScreen(navController: NavController, sharedViewModel: SharedViewModel
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
                 .padding(paddingValues)
                 .padding(16.dp)
         ) {
--- app/src/main/java/com/example/wigglesapp/SharedViewModel.kt
+++ app/src/main/java/com/example/wigglesapp/SharedViewModel.kt
@@ -4,22 +4,23 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class SharedViewModel: ViewModel() {
     private val _bookmarkedPets = MutableStateFlow<List<Pet>>(emptyList())
-    val bookmarkedPets: StateFlow<List<Pet>> = _bookmarkedPets
+    val bookmarkedPets: StateFlow<List<Pet>> = _bookmarkedPets.asStateFlow()
 
     private val _suggestedPets = MutableStateFlow<List<Pet>>(emptyList())
-    var suggestedPets: StateFlow<List<Pet>> = _suggestedPets
+    var suggestedPets: StateFlow<List<Pet>> = _suggestedPets.asStateFlow()
 
-    fun bookmarkPet(pet:Pet){
-        if(_bookmarkedPets.value.contains(pet).not()) {
-            _bookmarkedPets.value += pet
-        }
+    fun bookmarkPet(pet: Pet) {
+        _bookmarkedPets.value += pet
     }
-    fun removeBookmark(pet:Pet){
+
+    fun removeBookmark(pet: Pet) {
         _bookmarkedPets.value -= pet
     }
+
     fun setSuggestedPets(pets: List<Pet>) {
         _suggestedPets.value = pets
     }
