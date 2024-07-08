07ad97df1d4669dd30591e04a26e2d2aec6d3477
Mushmat <edu.chirayu2005@gmail.com>
2024-07-08 19:41:31 +0530

    Integrated the Suggested Pets Screen to display the preferred pet.

---
+++ app/src/main/java/com/example/wigglesapp/SuggestedPetsScreen.kt
@@ -0,0 +1,51 @@
+package com.example.wigglesapp
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SuggestedPetsScreen(navController: NavController, sharedViewModel: SharedViewModel){
+    val suggestedPets by sharedViewModel.suggestedPets.collectAsState(initial = emptyList())
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Suggested Pets") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(painter = painterResource(id = R.drawable.baseline_arrow_back_24), contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        if(suggestedPets.isEmpty()){
+            Text(text = "No pets found matching your preference!", fontSize = 20.sp,modifier = Modifier.padding(16.dp))
+        }else{
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(16.dp)
+            ) {
+                items(suggestedPets) { pet ->
+                    PetCard(navController = navController, pet = pet)
+                }
+        }
+    }
+
+}
