c0e983585edd3d09d830f8fdc42cb057f9024432
Mushmat <edu.chirayu2005@gmail.com>
2024-07-08 12:42:58 +0530

    Added the Shared View Model for bookmarking and removing the bookmarks.

--- app/src/main/java/com/example/wigglesapp/SharedViewModel.kt
+++ app/src/main/java/com/example/wigglesapp/SharedViewModel.kt
@@ -1,2 +1,21 @@
 package com.example.wigglesapp
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class SharedViewModel: ViewModel() {
+    private val _bookmarkedPets = MutableStateFlow<List<Pet>>(emptyList())
+    val bookmarkedPets: StateFlow<List<Pet>> = _bookmarkedPets
+
+    fun bookmarkPet(pet:Pet){
+        if(_bookmarkedPets.value.contains(pet).not()) {
+            _bookmarkedPets.value += pet
+        }
+    }
+
+    fun removeBookmark(pet:Pet){
+        _bookmarkedPets.value -= pet
+    }
+}
