8621fa9b5fb0d620199e3bb5595b4384df41d0e2
Mushmat <edu.chirayu2005@gmail.com>
2024-07-08 12:30:30 +0530

    Changed the Manifests file to revoke the hilt application.

--- app/src/main/java/com/example/wigglesapp/BookmarkedPetsScreen.kt
+++ app/src/main/java/com/example/wigglesapp/BookmarkedPetsScreen.kt
@@ -1,26 +1,19 @@
 package com.example.wigglesapp
 
-
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.DrawerState
-
-
 import androidx.compose.material3.Text
-
 import androidx.compose.runtime.Composable
-
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
 import androidx.compose.ui.platform.LocalContext
-
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import kotlinx.coroutines.CoroutineScope
 
-
 @Composable
-fun BookmarkedPetsScreen(navController: NavController, drawerState: DrawerState, scope: CoroutineScope) {
+fun BookmarkedPetsScreen(navController: NavController, drawerState: DrawerState, scope: CoroutineScope){
     val context = LocalContext.current
 
     Column(
@@ -28,7 +21,8 @@ fun BookmarkedPetsScreen(navController: NavController, drawerState: DrawerState,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Bookmark")
+        Text(text = "Bookmarked Pets Screen")
 
     }
+
 }
