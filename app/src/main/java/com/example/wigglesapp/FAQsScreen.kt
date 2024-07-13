a689b815a38bdefb8b5e20c36ebbe3fe805cd801
Mushmat <edu.chirayu2005@gmail.com>
2024-07-13 12:06:58 +0530

    Created the FAQs screen with the questions and answers in a scrollable card format.

--- app/src/main/java/com/example/wigglesapp/FAQsScreen.kt
+++ app/src/main/java/com/example/wigglesapp/FAQsScreen.kt
@@ -1,9 +1,32 @@
 package com.example.wigglesapp
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FAQsScreen(navController: NavController){
     val faqs = listOf(
@@ -19,5 +42,47 @@ fun FAQsScreen(navController: NavController){
         "How can I reset my password?" to "To reset your password, click on the 'Forgot Password' link on the login screen, enter your registered email address, and follow the instructions sent to your email."
     )
 
-
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "FAQs") },
+                navigationIcon = {
+                    IconButton(onClick = {navController.popBackStack()}) {
+                        Icon(painter = painterResource(id = R.drawable.baseline_arrow_back_24), contentDescription = "BACK")
+                    }
+                }
+            )
+        }
+    ){ paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(faqs) { faq ->
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = MaterialTheme.shapes.medium
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalAlignment = Alignment.Start
+                    ){
+                        Text(
+                            text = faq.first,
+                            style = MaterialTheme.typography.bodyLarge.copy(fontSize = 18.sp, color = Color.Black),
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = faq.second,
+                            style = MaterialTheme.typography.bodyMedium.copy(fontSize = 16.sp,color = Color.Gray)
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
