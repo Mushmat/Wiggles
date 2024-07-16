ab12307f593084a71a124a59636bd2b60926521c
Mushmat <edu.chirayu2005@gmail.com>
2024-07-16 12:50:29 +0530

    Changed the design of about us screen and pet detail screen for better readability

--- app/src/main/java/com/example/wigglesapp/AboutUsScreen.kt
+++ app/src/main/java/com/example/wigglesapp/AboutUsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.wigglesapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -8,6 +9,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -19,28 +22,28 @@ import androidx.navigation.NavController
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutUsScreen(navController: NavController) {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text(text = "Fur-tastic About Us", color = Color(0xFF000000)) },
-                    navigationIcon = {
-                        IconButton(onClick = { navController.popBackStack() }) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.baseline_arrow_back_24),
-                                contentDescription = "Back"
-                            )
-                        }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Fur-tastic About Us", color = Color(0xFF000000)) },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_arrow_back_24),
+                            contentDescription = "Back"
+                        )
                     }
-                )
-            }
-        ) { paddingValues ->
-            Box(modifier = Modifier.fillMaxSize()) {
-                Image(
-                    painter = painterResource(id = R.drawable.bg),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.fillMaxSize()
-                )
+                }
+            )
+        }
+    ) { paddingValues ->
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.bg),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -55,43 +58,55 @@ fun AboutUsScreen(navController: NavController) {
                     contentDescription = null,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(100.dp)
+                        .height(200.dp)
+                        .clip(MaterialTheme.shapes.medium)
+                        .padding(16.dp)
                 )
                 Spacer(modifier = Modifier.height(16.dp))
-                Text(text = "Welcome to Wiggles", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                Text(
+                    text = "Welcome to Wiggles",
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF6200EE)
+                )
                 Spacer(modifier = Modifier.height(8.dp))
                 Text(
                     text = "Your one-stop solution for finding and adopting the perfect pet! Our mission is to connect loving homes with pets in need and ensure that every pet finds a forever home. At Wiggles, we believe in the transformative power of pet adoption and strive to make the process as seamless and joyful as possible.",
-                    fontSize = 16.sp, color = Color(0xff1a1a73),
-                    modifier = Modifier.padding(horizontal = 8.dp)
+                    fontSize = 16.sp,
+                    color = Color(0xff1a1a73),
+                    modifier = Modifier.padding(horizontal = 16.dp)
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 SectionHeader(title = "Paws for a Cause")
                 Text(
                     text = "Our mission is to provide a platform that facilitates the adoption process, making it easier for you to find a pet that matches your lifestyle and preferences. We aim to:\n\n- Promote Pet Adoption\n- Provide Comprehensive Information\n- Support Pet Owners",
-                    fontSize = 16.sp, color = Color(0xff1a1a73),
-                    modifier = Modifier.padding(horizontal = 8.dp)
+                    fontSize = 16.sp,
+                    color = Color(0xff1a1a73),
+                    modifier = Modifier.padding(horizontal = 16.dp)
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 SectionHeader(title = "Tail Tales")
                 Text(
                     text = "At Wiggles, we offer a range of features designed to help you find and adopt the perfect pet:\n\n- Detailed Pet Profiles\n- Personalized Recommendations\n- Pet Care Tips\n- Nearby Vets\n- Community Support",
-                    fontSize = 16.sp, color = Color(0xff1a1a73),
-                    modifier = Modifier.padding(horizontal = 8.dp)
+                    fontSize = 16.sp,
+                    color = Color(0xff1a1a73),
+                    modifier = Modifier.padding(horizontal = 16.dp)
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 SectionHeader(title = "Paws & Perks")
                 Text(
                     text = "User-Friendly Interface\nComprehensive Resources\nCommitment to Pet Welfare",
-                    fontSize = 16.sp, color = Color(0xff1a1a73),
-                    modifier = Modifier.padding(horizontal = 8.dp)
+                    fontSize = 16.sp,
+                    color = Color(0xff1a1a73),
+                    modifier = Modifier.padding(horizontal = 16.dp)
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 SectionHeader(title = "Get in Touch with Paws")
                 Text(
                     text = "If you have any questions or need assistance, feel free to reach out to us at contact@wigglesapp.com. We're here to help!",
-                    fontSize = 16.sp, color = Color(0xff1a1a73),
-                    modifier = Modifier.padding(horizontal = 8.dp)
+                    fontSize = 16.sp,
+                    color = Color(0xff1a1a73),
+                    modifier = Modifier.padding(horizontal = 16.dp)
                 )
             }
         }
@@ -102,8 +117,17 @@ fun AboutUsScreen(navController: NavController) {
 fun SectionHeader(title: String) {
     Text(
         text = title,
-        fontSize = 20.sp,
+        fontSize = 22.sp,
         fontWeight = FontWeight.Bold,
-        modifier = Modifier.padding(vertical = 8.dp), color = Color(0xFF000000)
+        color = Color(0xFF6200EE),
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .background(
+                brush = Brush.horizontalGradient(
+                    colors = listOf(Color(0xFFFFA726), Color(0xFFFF7043))
+                )
+            )
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp)
     )
 }
--- app/src/main/java/com/example/wigglesapp/PetDetailScreen.kt
+++ app/src/main/java/com/example/wigglesapp/PetDetailScreen.kt
@@ -2,14 +2,7 @@ package com.example.wigglesapp
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -32,6 +25,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -40,8 +34,8 @@ import com.google.firebase.auth.FirebaseAuth
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PetDetailScreen(navController: NavController, petId: Int, sharedViewModel: SharedViewModel){
-    val pet = dummyPets.firstOrNull{ it.id == petId} ?: return
+fun PetDetailScreen(navController: NavController, petId: Int, sharedViewModel: SharedViewModel) {
+    val pet = dummyPets.firstOrNull { it.id == petId } ?: return
     val bookmarkedPets by sharedViewModel.bookmarkedPets.collectAsState()
 
     val isBookmarked = bookmarkedPets.any { it.id == pet.id }
@@ -53,97 +47,144 @@ fun PetDetailScreen(navController: NavController, petId: Int, sharedViewModel: S
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        TopAppBar(
-            title = { Text(text = "Paw Details") },
-            navigationIcon = {
-                IconButton(onClick = { navController.popBackStack() }) {
-                    Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
-                }
-            }
-        )
-
-        LazyColumn(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp)
         ) {
-            item {
-                Image(
-                    painter = rememberAsyncImagePainter(model = pet.imageUrl),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .aspectRatio(1.5f)
-                        .padding(bottom = 16.dp)
-                )
-            }
-
-            item {
-
-                Text(text = pet.name, fontSize = 32.sp, color = Color.Black, style = MaterialTheme.typography.labelLarge)
-                Spacer(modifier = Modifier.height(8.dp))
-
-                Text(text = "Breed: ${pet.breed}", fontSize = 20.sp, color = Color(0xFF5d4037))
-                Spacer(modifier = Modifier.height(8.dp))
-
-                Text(text = "Gender: ${pet.gender}", fontSize = 20.sp, color = Color(0xFF5d4037))
-                Spacer(modifier = Modifier.height(8.dp))
-
-                Text(text = "Size: ${pet.size}", fontSize = 20.sp, color = Color(0xFF5d4037))
-                Spacer(modifier = Modifier.height(8.dp))
-
-                Text(text = "Characteristics: ${pet.characteristics}", fontSize = 18.sp, color = Color(0xFF5d4037))
-                Spacer(modifier = Modifier.height(8.dp))
-
-                Text(text = "About: ${pet.about}", fontSize = 18.sp, color = Color(0xFF5d4037))
-                Spacer(modifier = Modifier.height(16.dp))
-    
-                Button(onClick = { 
-                if(isBookmarked){
-                    sharedViewModel.removeBookmark(pet.toBookmarkedPet())
-                }else{
-                    sharedViewModel.bookmarkPet(pet.toBookmarkedPet())
+            TopAppBar(
+                title = { Text(text = "Paw Details") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                    }
                 }
-                },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp)
-                        .clip(RoundedCornerShape(12.dp))
-                        .background(
-                            brush = Brush.horizontalGradient(
-                                listOf(Color(0xFFFFA726), Color(0xFFFF7043))
+            )
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                item {
+                    Image(
+                        painter = rememberAsyncImagePainter(model = pet.imageUrl),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(1.5f)
+                            .padding(bottom = 16.dp)
+                            .clip(RoundedCornerShape(16.dp))
+                            .background(Color.White)
+                            .shadow(8.dp, RoundedCornerShape(16.dp))
+                    )
+                }
+
+                item {
+                    Text(
+                        text = pet.name,
+                        fontSize = 32.sp,
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(vertical = 8.dp)
+                    )
+
+                    Text(
+                        text = "Breed: ${pet.breed}",
+                        fontSize = 20.sp,
+                        color = Color(0xFF5d4037),
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.padding(vertical = 4.dp)
+                    )
+
+                    Text(
+                        text = "Gender: ${pet.gender}",
+                        fontSize = 20.sp,
+                        color = Color(0xFF5d4037),
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.padding(vertical = 4.dp)
+                    )
+
+                    Text(
+                        text = "Size: ${pet.size}",
+                        fontSize = 20.sp,
+                        color = Color(0xFF5d4037),
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.padding(vertical = 4.dp)
+                    )
+
+                    Text(
+                        text = "Characteristics: ${pet.characteristics}",
+                        fontSize = 18.sp,
+                        color = Color(0xFF5d4037),
+                        fontWeight = FontWeight.Normal,
+                        modifier = Modifier.padding(vertical = 4.dp)
+                    )
+
+                    Text(
+                        text = "About: ${pet.about}",
+                        fontSize = 18.sp,
+                        color = Color(0xFF5d4037),
+                        fontWeight = FontWeight.Normal,
+                        modifier = Modifier.padding(vertical = 4.dp)
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Button(
+                        onClick = {
+                            if (isBookmarked) {
+                                sharedViewModel.removeBookmark(pet.toBookmarkedPet())
+                            } else {
+                                sharedViewModel.bookmarkPet(pet.toBookmarkedPet())
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                            .clip(RoundedCornerShape(12.dp))
+                            .background(
+                                brush = Brush.horizontalGradient(
+                                    listOf(Color(0xFFFFA726), Color(0xFFFF7043))
+                                )
                             )
-                        )
-                        .shadow(8.dp, RoundedCornerShape(12.dp)),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
+                            .shadow(8.dp, RoundedCornerShape(12.dp)),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
                     ) {
-                    Text(text = if(isBookmarked) "Remove Bookmark" else "Bookmark")
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                
-                Button(
-                    onClick = { navController.navigate("adoption_application/${pet.id}") },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp)
-                        .clip(RoundedCornerShape(12.dp))
-                        .background(
-                            brush = Brush.horizontalGradient(
-                                listOf(Color(0xFFFFA726), Color(0xFFFF7043))
-                            )
+                        Text(
+                            text = if (isBookmarked) "Remove Bookmark" else "Bookmark",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
                         )
-                        .shadow(8.dp, RoundedCornerShape(12.dp)),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
-                ) {
-                    Text(text = "Adopt ${pet.name}")
+                    }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Button(
+                        onClick = { navController.navigate("adoption_application/${pet.id}") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                            .clip(RoundedCornerShape(12.dp))
+                            .background(
+                                brush = Brush.horizontalGradient(
+                                    listOf(Color(0xFFFFA726), Color(0xFFFF7043))
+                                )
+                            )
+                            .shadow(8.dp, RoundedCornerShape(12.dp)),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
+                    ) {
+                        Text(
+                            text = "Adopt ${pet.name}",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                    }
                 }
             }
         }
-    }}
+    }
 }
 
 fun Pet.toBookmarkedPet(): BookmarkedPet {
