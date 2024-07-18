9c9a9b26edeed84db5c3edd712ccf5c235ad92b5
Mushmat <edu.chirayu2005@gmail.com>
2024-07-18 21:04:21 +0530

    Added the animation logic to pop up on the home screen

--- app/src/main/java/com/example/wigglesapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/wigglesapp/ui/screens/HomeScreen.kt
@@ -1,7 +1,16 @@
 package com.example.wigglesapp.ui.screens
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -13,8 +22,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -31,9 +45,12 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController, drawerState: DrawerState, scope: CoroutineScope, authViewModel: AuthViewModel) {
-    // Get the current context
-    val authState by authViewModel.authState.collectAsState()
     // Get the authentication state from the view model
+    val authState by authViewModel.authState.collectAsState()
+
+    //Variable to store the state of greeting animation display
+    var showGreeting by remember { mutableStateOf(true) }
+
     // Check the authentication state and navigate to the auth screen if not authenticated
     LaunchedEffect(key1 = authState.isAuthenticated) {
         if(!authState.isAuthenticated){
@@ -59,7 +76,9 @@ fun HomeScreen(navController: NavController, drawerState: DrawerState, scope: Co
         }
     ) { paddingValues ->
         // Main content container
-        Box(modifier = Modifier.fillMaxSize().padding(paddingValues)) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)) {
             // Background image
             Image(
                 painter = painterResource(id = R.drawable.bg),
@@ -102,6 +121,37 @@ fun HomeScreen(navController: NavController, drawerState: DrawerState, scope: Co
                     onClick = { /* Navigate to Donate */ })
             }
             Spacer(modifier = Modifier.height(16.dp))
+        }
+
+        AnimatedVisibility(
+            visible = showGreeting,
+            enter = fadeIn(animationSpec = tween(1000)) + slideInVertically(initialOffsetY = { it / 2}, animationSpec = tween(1000)),
+            exit = fadeOut(animationSpec = tween(1000)) + slideOutVertically(targetOffsetY = { it / 2 }, animationSpec = tween(1000))
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black.copy(alpha = 0.5f))
+                    .clickable { showGreeting = false },
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(Color.White)
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                )
+                {
+                    Image(
+                        painter = painterResource(id = R.drawable.animated_dog_image),
+                        contentDescription = "Dog Icon",
+                        modifier = Modifier.size(64.dp)
+                    )
+                    Text(text = "Hey, it's good to see you!", fontSize = 20.sp, color = Color.Black)
+                }
+            }
         }
     }
 }
