408cfc8db397ba0f4f08790c1c1d53121bb6afad
Mushmat <edu.chirayu2005@gmail.com>
2024-07-19 14:27:17 +0530

    Revoked animation

--- app/src/main/java/com/example/wigglesapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/wigglesapp/ui/screens/HomeScreen.kt
@@ -1,16 +1,8 @@
 package com.example.wigglesapp.ui.screens
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
+
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -18,20 +10,11 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
@@ -40,26 +23,20 @@ import androidx.navigation.NavController
 import com.example.wigglesapp.viewmodels.AuthViewModel
 import com.example.wigglesapp.R
 import com.example.wigglesapp.ui.components.HomeButton
-import com.example.wigglesapp.utils.PreferencesHelper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController, drawerState: DrawerState, scope: CoroutineScope, authViewModel: AuthViewModel) {
-    //CONTEXT
-    val context = LocalContext.current
     // Get the authentication state from the view model
     val authState by authViewModel.authState.collectAsState()
 
-    //Variable to store the state of greeting animation display
-    var showGreeting by remember { mutableStateOf(!PreferencesHelper.isGreetingShown(context)) }
-
     // Check the authentication state and navigate to the auth screen if not authenticated
     LaunchedEffect(key1 = authState.isAuthenticated) {
-        if(!authState.isAuthenticated){
-            navController.navigate("auth"){
-                popUpTo(navController.graph.startDestinationId) {inclusive = true}
+        if (!authState.isAuthenticated) {
+            navController.navigate("auth") {
+                popUpTo(navController.graph.startDestinationId) { inclusive = true }
             }
         }
     }
@@ -80,9 +57,11 @@ fun HomeScreen(navController: NavController, drawerState: DrawerState, scope: Co
         }
     ) { paddingValues ->
         // Main content container
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(paddingValues)) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
             // Background image
             Image(
                 painter = painterResource(id = R.drawable.bg),
@@ -126,40 +105,5 @@ fun HomeScreen(navController: NavController, drawerState: DrawerState, scope: Co
             }
             Spacer(modifier = Modifier.height(16.dp))
         }
-
-        AnimatedVisibility(
-            visible = showGreeting,
-            enter = fadeIn(animationSpec = tween(1000)) + slideInVertically(initialOffsetY = { it / 2}, animationSpec = tween(1000)),
-            exit = fadeOut(animationSpec = tween(1000)) + slideOutVertically(targetOffsetY = { it / 2 }, animationSpec = tween(1000))
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Black.copy(alpha = 0.5f))
-                    .clickable {
-                        showGreeting = false
-                               PreferencesHelper.setGreetingShown(context, true)
-                               },
-                contentAlignment = Alignment.BottomStart
-            ) {
-                Column(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .clip(RoundedCornerShape(8.dp))
-                        .background(Color.White)
-                        .padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                )
-                {
-                    Image(
-                        painter = painterResource(id = R.drawable.animated_dog_image),
-                        contentDescription = "Dog Icon",
-                        modifier = Modifier.size(64.dp)
-                    )
-                    Text(text = "Hey, it's good to see you!", fontSize = 20.sp, color = Color.Black)
-                }
-            }
-        }
     }
 }
-
